f1aa720aa3ba3866e3d5f51ac8d123b3c8dc9aa3
LUIGUI <luiguicitoalain@gmail.com>
2023-07-25 16:06:29 -0500

    Tercer commit de <Nombres y Apellidos>

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -1,23 +1,20 @@
 <?php
 
 namespace Database\Factories;
-
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
- */
 class PostFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
-    public function definition(): array
+    public function definition()
     {
-        return [
-            //
-        ];
-    }
+    return [
+        'user_id' => User::inRandomOrder()->first()->id,
+        'titulo' => $this->faker->sentence,
+        'slug' => $this->faker->unique()->slug,
+        'contenido' => $this->faker->text,
+    ];
+}
 }
